b9ed9729adf1746bcc6afee5eb52b0d40b4ab600
TinkeshDeshmukh <anuragdeshmukh108@gmail.com>
2025-08-05 20:26:59 +0530

    Fix: use server in production route

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute);
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("/*",(req,res)=>{
+    app.get("/*path",(req,res)=>{
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
 }
